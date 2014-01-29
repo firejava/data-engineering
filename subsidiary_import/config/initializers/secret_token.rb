ff81d247ac753b25bafd37d526aa368476f3a337
Chris Gibson <chrislgibson@gmail.com>
2014-01-28 19:53:59 -0600

    Add initial Rails application.

---
+++ subsidiary_import/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SubsidiaryImport::Application.config.secret_key_base = 'f4a3b35470fc20912c2f268cc36dde4e0ab8a4db48a244360cbae68edb3e3c3688d8e51d9bf5825cfd095ef4f2fe9a47e70bcbfb342c31dc583e3798d3e7d796'
---
+++ subsidiary_import/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SubsidiaryImport::Application.config.session_store :cookie_store, key: '_subsidiary_import_session'
